f3a9e4a2c6ee4b688b25931545adb781a09acd05
YeonKyoung <104209685+ryuyeonkyoung@users.noreply.github.com>
2024-08-22 14:06:37 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        // 타뷸레이션
+        long[] dp = new long[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        for (int i = 5; i < 101; i++) {
+            dp[i] = dp[i-5] + dp[i-1];
+        }
+        
+        // dfs
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
