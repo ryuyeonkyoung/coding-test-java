6a4397527c6d85264705748ef73378ee01b672c2
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-01-30 15:47:22 +0900

    [level 1] Title: 폰켓몬, Time: 4.06 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,23 +1,18 @@
 import java.util.*;
 
 // 1. 해시에 종류별로 포켓몬 정리하기
-// 2. 가장 많이 선택할 수 있는 종류 번호의 개수 리턴..? (최댓값 n/2, 이외에는 key의 개수)
+// 2. 가장 많이 선택할 수 있는 종류 번호의 개수 반환 (최댓값 n/2, 이외에는 key의 개수)
 class Solution {
     public int solution(int[] nums) {
         Map<Integer, Integer> ponkets = new HashMap<>();
         
         for (int n : nums) {
-            // key가 있으면 value+1
-            // key가 없으면 key,value 생성
-            if (ponkets.containsKey(n))
-                ponkets.put(n, ponkets.get(n) + 1);
-            else
-                ponkets.put(n, 1);
+            // value의 개수를 셀 필요가 없으니 덮어씌워도 된다.
+            ponkets.put(n, 1);
         }
         
-        if (ponkets.size() > nums.length/2)
-            return nums.length/2;
-        else return ponkets.size();
+        // if문을 삼항연산자로
+        return ponkets.size() > nums.length / 2 ? nums.length / 2 : ponkets.size();
     }
     
     
