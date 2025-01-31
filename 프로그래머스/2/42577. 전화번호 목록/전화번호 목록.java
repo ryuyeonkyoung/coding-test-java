93c8d27939a96a7090439bf199cffeabb084cb33
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-01-31 17:57:06 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,18 +3,11 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-
-        // 중간에 있으면 안되고 접두사로 있어야함
-        for (int i = 0; i < phone_book.length; i++) {
-            for (int j = i + 1; j < phone_book.length; j++) {
-                if (phone_book[j].contains(phone_book[i])
-                    && phone_book[j].indexOf(phone_book[i]) == 0) {
-                    return false;
-                } else break;
-            }
-
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) { return false; }
         }
-
+        
         return true;
     }
 }
