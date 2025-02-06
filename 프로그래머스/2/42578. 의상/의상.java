8f9bc4b9b51aced973085af249fc2e885f18c50c
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-02-06 14:53:04 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+    
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] cloth : clothes) {
+            if (!map.containsKey(cloth[1]))
+                map.put(cloth[1], 1);
+            else map.put(cloth[1], map.get(cloth[1]) + 1);
+        }
+        
+        for (int value : map.values()) {
+            answer *= (value + 1);
+        }
+        return answer - 1;
+    }
+}
