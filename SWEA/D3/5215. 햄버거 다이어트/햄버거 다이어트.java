95cfdf7244b1c300d84da7868acd40facdc9d287
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-05-22 23:21:29 +0900

    [D3] Title: 햄버거 다이어트, Time: 216 ms, Memory: 40,384 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -7,47 +7,10 @@ public class Solution {
 
 	public static int[] scores;
 	public static int[] calories;
-	public static boolean[] checked;
+	public static int[][] dp;
 	public static int N;
 	public static int L;
-	public static int max = 0;
 
-	public static void printCalories() {
-		for(int i=0; i<N; i++) {
-			if (checked[i]) System.out.println(calories[i]);
-		}
-	}
-
-	public static int getTotalCalories() {
-		int sum = 0;
-		for(int i=0; i<N; i++) {
-			if (checked[i]) sum += calories[i];
-		}
-		return sum;
-	}
-
-	public static int getTotalScore() {
-		int sum = 0;
-		for(int i=0; i<N; i++) {
-			if (checked[i]) sum += scores[i];
-		}
-		return sum;
-	}
-
-	public static void dfs(int start) {
-		if (getTotalCalories() > L) return;
-		else {
-			max = Math.max(max, getTotalScore());
-		}
-
-		for(int i=start; i<N; i++) {
-			if (!checked[i]) {
-				checked[i] = true;
-				dfs(i+1);
-				checked[i] = false;
-			}
-		}
-	}
 
 	public static void main(String[] args) throws Exception {
 //		System.setIn(new FileInputStream("res/5215_input.txt"));
@@ -55,21 +18,37 @@ public class Solution {
 
 		int T = sc.nextInt();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			max = 0;
 
+			// 입력
 			N = sc.nextInt();
 			L = sc.nextInt();
-			scores = new int[N];
-			calories = new int[N];
-			checked = new boolean[N];
-			for(int i=0; i<N; i++) {
+			scores = new int[N+1];
+			calories = new int[N+1];
+			dp = new int[N+1][L+1];
+			for(int i=1; i<=N; i++) {
 				scores[i] = sc.nextInt();
 				calories[i] = sc.nextInt();
 			}
 
-			dfs(0);
+			// dp[i][j] = value
+			// i는 1번째 재료부터 i번째 재료까지 고려한 경우
+			// j는 남은 칼로리
+			// value는 최대 score
+			// 재료를 하나씩 더해 나가면서, 모든 칼로리에 대한 최대 스코어를 갱신함.
+			for(int i=1; i<=N; i++) {
+				for(int remainKcal=1; remainKcal<=L; remainKcal++) {
+					// 1. 재료를 더해도 최대 칼로리를 넘지 않는지 확인한다.
+					if (remainKcal < calories[i]) {
+						dp[i][remainKcal] = dp[i-1][remainKcal];
+					} else {
+						dp[i][remainKcal] = Math.max(dp[i-1][remainKcal-calories[i]] + scores[i], dp[i-1][remainKcal]);
+					}
+				}
+			}
 
-			System.out.println("#" + test_case + " " + max);
+
+			// 출력
+			System.out.println("#" + test_case + " " + dp[N][L]);
 		}
 	}
 }
