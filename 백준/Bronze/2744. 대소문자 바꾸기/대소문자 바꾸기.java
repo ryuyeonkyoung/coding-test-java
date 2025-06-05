441077e421814d18e60d615a584ad86d450badf9
류연경 <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-06-05 12:41:36 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,6 +1,4 @@
-import java.util.*;           // List, Map, Set, Queue, Deque 등 대부분 자료구조
-import java.io.*;             // BufferedReader, BufferedWriter
-import java.util.StringTokenizer;  // 이건 따로! ⭐ 꼭 기억!
+import java.io.*;
 
 public class Main {
 
@@ -9,26 +7,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        String word = st.nextToken();
+        String word = br.readLine();
 
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
-            if (c >= 'a' && c <= 'z') {
-                // 소문자 -> 대문자
-                bw.write((char) (c - 32));
-            } else if (c >= 'A' && c <= 'Z') {
-                // 대문자 -> 소문자
-                bw.write((char) (c + 32));
-            } else {
-                // 알파벳이 아닌 경우 그대로 출력
-                bw.write(c);
-            }
+
+            if (Character.isUpperCase(c))
+                c = Character.toLowerCase(c);
+            else if (Character.isLowerCase(c))
+                c = Character.toUpperCase(c);
+
+            bw.write(c);
         }
 
         bw.flush();
-
     }
 }
