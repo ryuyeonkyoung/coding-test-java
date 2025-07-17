d91bc9f8bbc14500381e67fa19df0f15cfd73e3b
RyuYeonKyoung <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-07-17 23:31:34 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1676 ms, Memory: 59424 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int calculate(int n) {
+        int five = n/5;
+
+        if (n % 5 == 0)
+            return n/5;
+        while(five >= 0){
+            if ((n - 5*five) % 3 == 0)
+                return five + (n - 5*five) / 3;
+            else
+                five--;
+
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1])
+                    return o1[0]-o2[0];
+                else return o1[1]-o2[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+
+    }
+}
