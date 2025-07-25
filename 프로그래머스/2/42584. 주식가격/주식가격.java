1f757226207bec0c13c9fad85daaf5966dd39841
RyuYeonKyoung <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-07-25 15:56:49 +0900

    [level 2] Title: 주식가격, Time: 17.62 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] solution(int[] prices) {
         int length = prices.length;
         int[] answer = new int[length];
-
+        
         for (int i = 0; i < prices.length; i++) {
             // 중간에 더 작은 수가 나타나면 길이 측정
             boolean flag = false;
-            for (int j = i + 1; j < prices.length; j++) {
-                // System.out.println("i:" + i + "j:" + j);
+            for (int j = i + 1; j < length; j++) {
                 if (prices[i] > prices[j]) {
                     flag = true;
                     answer[i] = j - i;
@@ -16,7 +15,7 @@ class Solution {
             }
             // 마지막까지 작은 수가 없다면 마지막까지의 길이 측정
             if (!flag) {
-                answer[i] = prices.length - 1 - i;
+                answer[i] = length - 1 - i;
             }
         }
 
