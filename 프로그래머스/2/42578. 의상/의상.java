8c261154687bc7867b7668a8957df66f9ab15944
RyuYeonKyoung <104209685+ryuyeonkyoung@users.noreply.github.com>
2025-07-28 23:20:20 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,25 @@
 import java.util.*;
-    
+
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String[] cloth : clothes) {
-            if (!map.containsKey(cloth[1]))
-                map.put(cloth[1], 1);
-            else map.put(cloth[1], map.get(cloth[1]) + 1);
+        int answer = 0;
+        Map <String, LinkedList<String>> clothesMap = new HashMap<String, LinkedList<String>>();
+
+        for (String[] arr: clothes) {
+            // System.out.println("arr[1]: " + arr[1]);
+            // System.out.println("arr[0]: " + arr[0]);
+            if (clothesMap.get(arr[1]) == null) {
+                clothesMap.put(arr[1], new LinkedList<String>());
+            }
+            clothesMap.get(arr[1]).add(arr[0]);
         }
-        
-        for (int value : map.values()) {
-            answer *= (value + 1);
+
+        int cnt = 1;
+        for (LinkedList<String> list: clothesMap.values()) {
+            // System.out.println(list.size());
+            cnt *= list.size() + 1;
         }
-        return answer - 1;
+
+        return cnt - 1;
     }
 }
